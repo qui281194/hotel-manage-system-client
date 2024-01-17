2e884821b72505ac296b282cab972298bce57ac7
ASUS <ASUS@DESKTOP-JIS65OV>
2024-01-18 00:22:54 +0700

    upload feedback da sua doi

--- src/main/java/fpt/aptech/hotelclient/controller/admin/FeedbackController.java
+++ src/main/java/fpt/aptech/hotelclient/controller/admin/FeedbackController.java
@@ -11,7 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -36,6 +39,29 @@ public class FeedbackController {
 
         // Add the feedback list to the model for rendering in the view
         model.addAttribute("feedbacks", feedbackList);
+
+        // Return the name of the view to be rendered
+        return "admin/contact/index";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteFeedback(@PathVariable Integer id) {
+        // Make a DELETE request to delete the feedback with the specified ID
+        restTemplate.delete(apiUrl + "delete/" + id);
+
+        // Redirect to the feedback list page after deletion
+        return "redirect:/admin/feedbacks/all";
+    }
+    @PostMapping("/search")
+    public String searchFeedback(@RequestParam("sender") String sender, Model model) {
+        // Gửi yêu cầu tìm kiếm đến server và nhận danh sách kết quả
+        ResponseEntity<Feedback[]> responseEntity = restTemplate.getForEntity(apiUrl + "search?sender=" + sender, Feedback[].class);
+        Feedback[] searchedFeedbacks = responseEntity.getBody();
+
+        // Convert array to List for easier handling in the view
+        List<Feedback> searchedFeedbackList = Arrays.asList(searchedFeedbacks);
+
+        // Add the searched feedback list to the model for rendering in the view
+        model.addAttribute("feedbacks", searchedFeedbackList);
 
         // Return the name of the view to be rendered
         return "admin/contact/index";
